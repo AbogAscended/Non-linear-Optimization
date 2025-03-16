a3f595c7b4cdda3dc425ddab34eb2e97640d372f
AbogAscended <jlevine@cpp.edu>
2025-03-16 11:31:22 -0700

    #

---
+++ NewtonsMethod/MultiVariable.py
@@ -0,0 +1,61 @@
+"""
+Let f(x1,x2) = 2x1^4 + x2^2 - 4x1x2 + 5x2
+"""
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+
+# Setup our variables and function using sympy
+x1, x2 = sp.symbols('x1 x2')
+f = 2*(x1**4) + x2**2 - 4*x1*x2 + 5*x2
+
+# Use sympy to calculate our gradient and hessian symbolically
+gradient = [sp.diff(f,var) for var in (x1,x2)]
+hessian  = [[sp.diff(g,var) for var in (x1,x2)] for g in gradient]
+
+# Convert our symbolic solutions to lambda functions for solving
+fn = sp.lambdify((x1,x2), f, 'numpy')
+grad_fn = sp.lambdify((x1,x2), gradient, 'numpy')
+hess_fn = sp.lambdify((x1,x2), hessian, 'numpy')
+
+# Implementation of multivariable newtons method
+x = np.zeros((10,2), dtype= np.float64)
+for i in range(1,x.shape[0]):
+    x[i:] = x[i-1,:] - np.linalg.inv(hess_fn(x[i-1,0],x[i-1,1]))@grad_fn(x[i-1,0],x[i-1,1])
+    print(f'x^({i}) = {x[i,:]}')
+"""
+Output:
+x^(1) = [1.25 0.  ]
+x^(2) = [ 0.72033898 -1.05932203]
+x^(3) = [-0.90260633 -4.30521267]
+x^(4) = [-1.88402029 -6.26804059]
+x^(5) = [-1.5157418 -5.5314836]
+x^(6) = [-1.39412209 -5.28824418]
+x^(7) = [-1.38057119 -5.26114239]
+x^(8) = [-1.38040894 -5.26081788]
+x^(9) = [-1.38040892 -5.26081783]
+"""
+
+
+x1Vals = np.linspace(-7,2,4000)
+x2Vals = np.linspace(-7,2,4000)
+X1, X2 = np.meshgrid(x1Vals,x2Vals)
+Z = fn(X1,X2)
+
+plt.figure(figsize=(8, 6))
+plt.contour(X1, X2, Z, levels=25, cmap='viridis')
+plt.colorbar(label='f(x1, x2)')
+
+plt.plot(x[:, 0], x[:, 1], 'ro-', label="Newton's Iterates", linewidth=2, markersize=8)
+plt.scatter(x[0, 0], x[0, 1], color='blue', label='Initial Guess', zorder=2)
+plt.scatter(x[-1, 0], x[-1, 1], color='green', label='Final Estimate', zorder=2)
+
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.title("Convergence of Newton's Method")
+plt.legend()
+plt.grid(True)
+plt.savefig('NewtonsMethod/graphs/multicontour.png')
+
+
+
